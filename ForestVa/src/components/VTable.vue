ed831d645153e7ae2bc4a5bf3cc80452cbc1b391
Carturo <anon02carlos@gmail.com>
2024-05-28 23:36:06 -0400

    VTable con exportar a PDF pero hay que instalar dependencias html2pdf.js

--- ForestVa/src/components/VTable.vue
+++ ForestVa/src/components/VTable.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import Button from 'primevue/button'
+import Button from 'primevue/button';
 import useEvents from '@/common/utils/useEvents';
-import { ref } from 'vue';
-import { VueElement } from 'vue';
 
 const props = defineProps({
     controller: Object,
     cb: Function,
     form: {
-        type: {
-            component: VueElement,
-            header: String
-        }
+        type: Object,
+        required: true
     },
     dataKey: String,
     edition: {
@@ -25,9 +22,11 @@ const props = defineProps({
         default: true
     }
 });
+import html2pdf from 'html2pdf.js';
 
 const { response } = props.controller.getElements();
 const editingRows = ref(!props.edition);
+const tableRef = ref(null); 
 
 function create() {
     useEvents().dispatch('showDialog', props.form);
@@ -37,25 +36,51 @@ function create() {
 function edit(event){
     props.controller.edit(event.newData);
 }
+
+const exportarTablaAPdf = () => {
+    if (!tableRef.value || !tableRef.value.$el || !tableRef.value.$el.children.length) {
+        console.error("null.");
+        return;
+    }
+    const clone = tableRef.value.$el.cloneNode(true);
+    const buttons = clone.querySelectorAll('button');
+    buttons.forEach(btn => btn.parentNode.removeChild(btn));
+
+    const options = {
+        filename: "datos.pdf",
+        image: { type: 'jpeg', quality: 1 },
+        html2canvas: { scale: 1 },
+        jsPDF: { unit: 'mm', format: 'a3', orientation: 'portrait' },
+    };
+
+   
+    html2pdf(clone, options);
+};
 </script>
 
 <template>
     <div class="card">
         <DataTable 
-        v-model:editingRows="editingRows"
-        editMode="row"
-        :value="response"
-        stripedRows 
-        tableStyle="min-width: 70rem"
-        :dataKey="props.dataKey"
-        @row-edit-save="edit"
+            ref="tableRef" 
+            v-model:editingRows="editingRows"
+            editMode="row"
+            :value="response"
+            stripedRows 
+            tableStyle="min-width: 70rem"
+            :dataKey="props.dataKey"
+            @row-edit-save="edit"
         >
             <Column>
                 <template #header>
-                    <Button class="btn" @click="create">
-                        {{$t('table.create')}}
-                        <i class="icon ion-plus"></i>
-                    </Button>
+                    <div class="button-container">
+                        <Button class="btn" @click="create">
+                            {{$t('table.create')}}
+                            <i class="icon ion-plus"></i>
+                        </Button>
+                        <div class="export_to_PDF" v-if="response && response.length > 0">
+                            <Button class="bt1" label="Exportar a PDF" @click="exportarTablaAPdf" :style="{ backgroundColor: 'red', 'border-radius': '10px', border: 'none'}"/>
+                        </div>
+                    </div>
                 </template>
             </Column>
             <slot></slot>
@@ -71,6 +96,7 @@ function edit(event){
     </div>
 </template>
 
+
 <style scoped>
 .card{
     align-items: center;
@@ -79,5 +105,11 @@ function edit(event){
 
 .btn{
     border-radius: 10px;
+    margin-bottom: 10px;
+}
+
+.button-container {
+    display: flex;
+    flex-direction: column;
 }
 </style>
